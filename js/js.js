ce2e0f0d4139cd2667c5aa59d44eade4e5d06cd4
Pavel Alemasov <palemasov001@gmail.com>
2023-07-21 00:52:39 +0500

    12 lesson 2 assignment

--- js/js.js
+++ js/js.js
@@ -1,4 +1,12 @@
 "use strict";
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?")
-console.log(numberOfFilms)
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+console.log(numberOfFilms);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+}
