1698e6fe2cd0d1440f830020dc5c9db5bff04c0a
Pavel Alemasov <palemasov001@gmail.com>
2023-07-21 01:10:08 +0500

    12 lesson 3 assignment

--- js/js.js
+++ js/js.js
@@ -1,12 +1,23 @@
 "use strict";
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 console.log(numberOfFilms);
 
-let personalMovieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     private: false
-}
+};
+
+const questionNumber1 = prompt('Один из последних просмотренных фильмов?', '');
+const questionNumber2 = prompt('Насколько оцените его?', '');
+const questionNumber3 = prompt('Один из последних просмотренных фильмов?', '');
+const questionNumber4 = prompt('Насколько оцените его?', '');
+
+personalMovieDB.movies[questionNumber1] = questionNumber2;
+personalMovieDB.movies[questionNumber3] = questionNumber4;
+
+console.log(personalMovieDB.movies[questionNumber1]);
+console.log(personalMovieDB.movies[questionNumber3]);
